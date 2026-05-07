2446c050b3c35b9b1fb8f325b73a6fc104bfa980
luanafariasrodrigues-ai <luanafariasrodrigues@gmail.com>
2026-05-06 23:43:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import pytesseract
+from PIL import Image, ImageEnhance
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# caminho do tesseract (ajusta se precisar)
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files.get("image")
+
+    if not file:
+        return "No file uploaded"
+
+    path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(path)
+
+    # processa imagem
+    img = Image.open(path)
+    img = ImageEnhance.Contrast(img).enhance(2)
+    img = img.convert("L")
+
+    # OCR
+    text = pytesseract.image_to_string(img, lang="por")
+
+    return f"<pre>{text}</pre>"
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
